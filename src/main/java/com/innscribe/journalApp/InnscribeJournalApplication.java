fae21bd1bf85a6950c42f8d691c3b9635e3c1c8f
DevanshSK <devanshs843@gmail.com>
2024-08-30 00:03:44 +0530

    Spring boot project initialized

---
+++ src/main/java/com/innscribe/journalApp/InnscribeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.innscribe.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnscribeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnscribeJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innscribe/journalApp/InnscribeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innscribe.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnscribeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
