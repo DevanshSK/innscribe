2a289aec084d4443499f87d93f0aa5f3cb65725f
DevanshSK <devanshs843@gmail.com>
2024-08-30 01:28:14 +0530

    Created a Journal REST API

--- src/main/java/com/innscribe/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/innscribe/journalApp/controller/JournalEntryController.java
@@ -1,13 +1,45 @@
 package com.innscribe.journalApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.innscribe.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
-    @GetMapping("/health-check")
-    public String healthCheck(){
-        return "OK";
+    // Create a local hashmap to store the journal entries.
+    private final Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAllJournalEntries(){
+        // Return all journal entries.
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createJournalEntry(@RequestBody JournalEntry journal){
+        // Create a new Journal Entry.
+        journalEntries.put(journal.getId(), journal);
+        return true;
+    }
+
+    @GetMapping("id/{myID}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myID){
+        return journalEntries.get(myID);
+    }
+
+    @DeleteMapping("id/{myID}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myID){
+        return journalEntries.remove(myID);
+    }
+
+    @PutMapping("id/{id}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long id, @RequestBody JournalEntry journal){
+        return journalEntries.put(id, journal);
     }
 }
